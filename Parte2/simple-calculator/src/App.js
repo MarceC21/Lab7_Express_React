a06a0107ca9d56d3f5ca5ce5faec2ce2bd01a3bf
MarceC21 <marcelap.castillo2102@gmail.com>
2026-05-05 18:20:03 -0600

    feat: calculadora funcionando con lo bàsico

--- Parte2/simple-calculator/src/App.js
+++ Parte2/simple-calculator/src/App.js
@@ -1,7 +1,32 @@
-import './App.css';
+// Se importa UseSate
+// Se importan los componentes Display y ButtonPanel
+import { useState } from "react";
+import Display from "./components/Display";
+import ButtonPanel from "./components/ButtonPanel";
+import "./App.css";
 
 function App() {
+    const [input, setInput] = useState('');
 
+    const handleButtonClick = (value) => {
+        if (value === '=') {
+            // Evaluate the expression
+            try {
+                setInput(eval(input).toString());
+            } catch (error) {
+                setInput('Error');
+            }
+        } else {
+            setInput(input + value);
+        }
+    };
+
+    return (
+        <div className="calculator">
+            <Display value={input} />
+            <ButtonPanel onButtonClick={handleButtonClick} />
+        </div>
+    );
 }
 
 export default App;
